7fa27aa967ba2e80d38b3dc213b7a14b271b275e
Byron Fanti <bfant@mybce.catholic.edu.au>
2024-06-13 19:19:03 +1000

    added tableTest.c

--- table.c
+++ table.c
@@ -134,22 +134,3 @@ void printTable(ht* table) {
     }
 }
 
-int main() {
-    ht* table = ht_init(5);
-    
-    htInsert(table, "1", "Test1");
-    htInsert(table, "2", "Test2");
-    htInsert(table, "3", "Test3");
-    htRemove(table, "2");
-    
-    htInsert(table, "2", "Test4");
-
-    htResize(table, 6);
-    
-    htInsert(table, "4", "Test5");
-
-    printTable(table);
-
-    return 0;
-}
-
